d80adaefacc6499271aec23ebb3e14260ca8d253
Zonny <zonny2609@gmail.com>
2022-07-28 13:43:29 -0400

    Lazyload basico listo

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -4,3 +4,4 @@ export const LazyPage1 = () => {
     <h1>LazyPage1</h1>
   )
 }
+export default LazyPage1
--- src/01-lazyload/pages/LazyPage2.tsx
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -4,3 +4,4 @@ export const LazyPage2 = () => {
     <h1>LazyPage2</h1>
   )
 }
+export default LazyPage2
